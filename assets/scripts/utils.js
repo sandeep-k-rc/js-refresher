d0194f3e9e3c8d2bd91c9a898d0ccf48cd151d56
Sandeep Kumar <205193407+sandeep-k-rc@users.noreply.github.com>
2025-07-09 21:03:13 +0530

    for loop variations

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,5 +1,44 @@
-export let apikey= "636sdjhsj838sdjh39";
-export let test= "testString";
 
-export default "this is the default export from utils.js";
+//const list = document.querySelector('ul');
+//list.remove();
 
+//for loop 
+const hobbies = ['Sports', 'Cooking', 'Reading']
+for (let i = 0; i < hobbies.length; i++) {
+    console.log(hobbies[i]);
+}
+
+// for of loop
+for (const hobby of hobbies) {
+    console.log(hobby);
+}
+
+// for in loop
+const person = {
+    name: 'Sandeep',
+    age: 30,
+    city: 'New York'
+};
+
+for (const key in person) {
+    console.log(key, person[key]);
+}
+
+// forEach loop
+hobbies.forEach((hobby) => {
+    console.log(hobby);
+});
+
+// while loop
+let i = 0;
+while (i < hobbies.length) {
+    console.log(hobbies[i]);
+    i++;
+}
+
+// do while loop
+i = 0;
+do {
+    console.log(hobbies[i]);
+    i++;
+} while (i < hobbies.length);
